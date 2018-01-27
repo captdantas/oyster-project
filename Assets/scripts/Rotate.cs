98731392bb9a7b56362f1979e3a3260098703c06
atillagallio <atilla.gallio@gmail.com>
2018-01-27 11:09:22 -0200

    intro commit

---
+++ Assets/scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+private bool turnedOn = true;
+public int speed = 1;
+
+    void FixedUpdate ()
+	{
+    	
+		// Rotate the object around its local X axis at 1 degree per second
+		if (turnedOn) {
+			transform.Rotate(Vector3.forward, Time.deltaTime * speed);
+		}
+
+
+    }
+}
+
---
+++ Assets/scripts/TextAnimator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAnimator : MonoBehaviour {
+
+	public float letterPause = 1.5f;
+
+     string message;
+     Text textComp;
+     public bool trigger = false;
+     private bool happenedTrigger = false;
+ 
+     // Use this for initialization
+     void Start () {
+         textComp = GetComponent<Text>();
+         message = textComp.text;
+         textComp.text = "";
+
+     }
+
+     void FixedUpdate ()
+	{
+		if (trigger && !happenedTrigger ) {
+			trigger = false;
+			happenedTrigger = true;
+
+			StartCoroutine(TypeText ());
+			Debug.Log("test");
+
+		}
+
+     }
+ 
+     IEnumerator TypeText () {
+         foreach (char letter in message.ToCharArray()) {
+             textComp.text += letter;
+
+             yield return new WaitForSeconds (letterPause);
+         }
+     }
+}
