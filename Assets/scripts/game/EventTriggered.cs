eb6218924a667e8c9076698487f5d1cbbf2f0d76
atillagallio <atilla.gallio@gmail.com>
2018-01-28 13:39:35 -0200

    Phrases changes

---
+++ Assets/scripts/game/EventTriggered.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTriggered{
+
+	public static List<int> VONA_EVENT = new List<int>();
+
+	public static void AddVonaEvent(int eventNumber){
+		VONA_EVENT.Add(eventNumber);
+	}
+
+}
