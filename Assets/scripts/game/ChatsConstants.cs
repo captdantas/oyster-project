be0d5d6d63ed60c5e9737a2f92848aec28c50a91
Márcio Dantas <marcio.sr.dantas@gmail.com>
2018-01-28 16:33:11 -0200

    Ajustes

--- Assets/scripts/game/ChatsConstants.cs
+++ Assets/scripts/game/ChatsConstants.cs
@@ -10,23 +10,23 @@ public class Constants {
 	public static bool CHARACTER_1_ENDING3 = false;
 	public static bool CHARACTER_1_DEATH = false;
 
-	public static bool CHARACTER_2_TEXT_1 = false;
-	public static bool CHARACTER_2_TEXT_2 = false;
-	public static bool CHARACTER_2_TEXT_3 = false;
+	public static bool CHARACTER_2_ENDING1 = false;
+	public static bool CHARACTER_2_ENDING2 = false;
+	public static bool CHARACTER_2_ENDING3 = false;
 	public static bool CHARACTER_2_DEATH = false;
 
-	public static bool CHARACTER_3_TEXT_1 = false;
-	public static bool CHARACTER_3_TEXT_2 = false;
-	public static bool CHARACTER_3_TEXT_3 = false;
+	public static bool CHARACTER_3_ENDING1 = false;
+	public static bool CHARACTER_3_ENDING2 = false;
+	public static bool CHARACTER_3_ENDING3 = false;
 	public static bool CHARACTER_3_DEATH = false;
 
-	public static bool CHARACTER_4_TEXT_1 = false;
-	public static bool CHARACTER_4_TEXT_2 = false;
-	public static bool CHARACTER_4_TEXT_3 = false;
+	public static bool CHARACTER_4_ENDING1 = false;
+	public static bool CHARACTER_4_ENDING2 = false;
+	public static bool CHARACTER_4_ENDING3 = false;
 	public static bool CHARACTER_4_DEATH = false;
 
-	public static bool CHARACTER_5_TEXT_1 = false;
-	public static bool CHARACTER_5_TEXT_2 = false;
-	public static bool CHARACTER_5_TEXT_3 = false;
+	public static bool CHARACTER_5_ENDING1 = false;
+	public static bool CHARACTER_5_ENDING2 = false;
+	public static bool CHARACTER_5_ENDING3 = false;
 	public static bool CHARACTER_5_DEATH = false;
 }
