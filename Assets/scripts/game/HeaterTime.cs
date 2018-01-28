e3ee71eef7497e9a30a668b603599c77811263ab
atillagallio <atilla.gallio@gmail.com>
2018-01-28 12:23:35 -0200

    Some changes to finish scene and Time Left script working

---
+++ Assets/scripts/game/HeaterTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HeaterTime : MonoBehaviour {
+
+	public GameObject text;
+
+	void OnMouseDown ()
+	{
+		if (text.activeSelf) {
+			text.SetActive (false);
+		} else {
+			text.SetActive(true);
+		}
+ 
+ }
+}
